7bb650540a5803e4e32577fd768cebd356ef093f
Mozzy9328 <74562419+Mozzy9328@users.noreply.github.com>
2021-05-26 14:25:55 +0200

    update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const dropDownContent = document.querySelector(".dropdown-content")
 
 
 function dropDown(){
+    
     myDropDown.classList.toggle("show")
 
     if (!event.target.matches(".dropbtn")) {
@@ -18,3 +19,85 @@ function dropDown(){
 }
 
 shoppingButton.addEventListener("click", dropDown)
+
+
+
+// Search function 
+
+const body = document.querySelector("body")
+const searchIcon = document.querySelector(".search-icon-click")
+
+
+
+function activateSearch() {
+    
+    body.innerHTML = `
+        <header>
+			<nav>
+				<ul class="top-header-container">
+					<li class="nav-1" id="fix-image">
+						<a href="index.html"> <img src="Images/Transparent Logo.png" /></a>
+					</li>
+					<li class="text-nav">
+						<a href="contact.html">Contact Us</a>
+					</li>
+					<li class="text-nav">
+						<div class="dropdown">
+							<button class="dropbtn">Shopping</button>
+							<div id="mydropdown" class="dropdown-content">
+								<a href="men.html">Men</a>
+								<a href="woman.html">Woman</a>
+							</div>
+						</div>
+					</li>
+					<li class="text-nav">
+						<a href="about.html">About Us</a>
+					</li>
+					<li class="nav-5">
+						<div>
+							<a href="search.html">
+								<i class="fas fa-search fa-2x fa-fw fa-fw"></i>
+							</a>
+						</div>
+						<div>
+							<i class="fas fa-user fa-2x fa-fw fa-fw"></i>
+						</div>
+						<div>
+							<a href="checkout-page.html"
+								><i class="fas fa-shopping-cart fa-2x fa-fw"></i
+							></a>
+						</div>
+					</li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<div class="search-button">
+				<input
+					type="text"
+					id="search"
+					placeholder="Search for items, brands and inspiration..."
+				/>
+				<div class="search-icon">
+					<i class="fas fa-search fa-2x fa-fw fa-fw"></i>
+				</div>
+			</div>
+            <div> 
+                <a href="index.html">
+                     <p class="back-button">Home Page</p>
+                </a>
+            </div>
+		</main>
+		<footer class="index-footer">
+			<p>Copyright &copy; 2020 Mustafa Ahmed | All Rights Reserved</p>
+		</footer>
+		<script src="js/script.js"></script>`
+}
+
+searchIcon.addEventListener("click", activateSearch)
+
+
+
+// Search not working 
+
+const clickedSearch = document.querySelector(".s")
