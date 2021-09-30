cdfca480a626a86131de4eb1bd623a30c183efc9
Mozzy9328 <74562419+Mozzy9328@users.noreply.github.com>
2021-09-30 12:57:05 +0200

    update

---
+++ js/hamburger.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector(".menu")
+const line1 = document.querySelector(".line1")
+const line2 = document.querySelector(".line2")
+const line3 = document.querySelector(".line3")
+
+const hamburgerMenu = document.querySelector(".hamburger-menu")
+
+hamburgerMenu.addEventListener("click", () =>{
+menu.classList.toggle("change")
+
+})
